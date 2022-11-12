7f82906bb948ce22eed94043b2771ef7ace43b76
Pail4 <73517684+Pail4@users.noreply.github.com>
2022-11-12 18:23:14 +0400

    mongoose added

--- src/schemas/movies.ts
+++ src/schemas/movies.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
 
-export const MovieSchema = new mongoose.Schema({ // определяем схему
+export const Movies =  new mongoose.Schema({ // определяем схему
     title: String,
+    category: String,
+    duration: Number,
+    director: String,
     year: Number,
     rating: Number,
 })
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import { MovieSchema } from '../schemas/movies'
+import * as schemas from '../schemas'
 import URL from './urls.json'
 
 mongoose.connect(URL.DATA_BASE)
@@ -16,8 +16,8 @@ app.get(ROUTES.BASE, (req, res) => {
 
 app.post(ROUTES.MOVIES, async (req, res) => {
     try {
-        const Movie = mongoose.model('Movie', MovieSchema)
-        await Movie.create(req.body)
+        const Movie = mongoose.model('Movie', schemas.Movies)
+        Movie.create(req.body)
     } catch (error) {
         res.status(400).send(error)
 
@@ -29,8 +29,8 @@ app.post(ROUTES.MOVIES, async (req, res) => {
 
 app.post(ROUTES.CATEGORIES, (req, res) => {
     try {
-        // const categories = client.db('main').collection('categories')
-        // categories.insertOne(req.body)
+        const Categories = mongoose.model('Categories', schemas.Categories)
+        Categories.create(req.body)
     } catch (error) {
         res.status(400).send(error)
 
@@ -38,4 +38,8 @@ app.post(ROUTES.CATEGORIES, (req, res) => {
     }
 
     return res.status(201).send('category created!')
+})
+
+app.listen(URL.PORT, () => {
+    console.log(`Example listening on port ${URL.PORT}`)
 })
